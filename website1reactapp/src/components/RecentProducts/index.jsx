4d0b77657ed374d230a315b1e6830ae880b28e73
JaleRehimli0909 <rehimlijale0909@gmail.com>
2023-11-08 13:35:19 +0400

    Recent Products added

---
+++ website1reactapp/src/components/RecentProducts/index.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import "./style.css";
+import { Card, Button, Col, Row } from "antd";
+const { Meta } = Card;
+
+const RecentProducts = () => {
+  const products = [
+    {
+      key: "1",
+      image: require("../../assets/images/img1.jpg"),
+      title:
+        "Fairy Clean & Fresh Washing Up Liquid Pomegranate &amp; Honeysuckle 520 ml",
+      price: "£2.00",
+    },
+    {
+      key: "2",
+      image: require("../../assets/images/img2.jpg"),
+      title: "Colgate Triple Action Toothpaste 100ml",
+      price: "£1.50",
+    },
+    {
+      key: "3",
+      image: require("../../assets/images/img3.jpg"),
+      title: "Breeze Toilet Tissue Soft White 24 Roll",
+      price: "£5.70",
+    },
+    {
+      key: "4",
+      image: require("../../assets/images/img4.jpg"),
+      title:
+        "Comfort intense Fresh Sky Fabric Conditioner Liquid 60 Wash 900 ML",
+      price: "£4.50",
+    },
+  ];
+  return (
+    <div className="recentProducts">
+      <h2>Recent Products</h2>
+      <Row gutter={[16, 16]}>
+        {products.map((product) => {
+          return (
+            <Col span={6} key={product.key}>
+              <Card
+                hoverable
+                style={{
+                  width: 300,
+                }}
+                cover={
+                  <img
+                    alt="example"
+                    className="productsImg"
+                    src={product.image}
+                  />
+                }
+              >
+                {/* <Meta title={product.title} /> */}
+
+                <h3 className="productsTitle">{product.title}</h3>
+                <p className="price">{product.price}</p>
+                <Button className="cardButton" type="primary">
+                  Add to Basket
+                </Button>
+              </Card>
+            </Col>
+          );
+        })}
+      </Row>
+    </div>
+  );
+};
+
+export default RecentProducts;
--- website1reactapp/src/pages/Home/index.jsx
+++ website1reactapp/src/pages/Home/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./style.css";
 import CarouselApp from '../../components/CarouselApp';
+import RecentProducts from "../../components/RecentProducts";
 
 const Home = () => {
   return (
     <div className="container">
       <CarouselApp/>
+      <RecentProducts/>
   
     </div>
 
