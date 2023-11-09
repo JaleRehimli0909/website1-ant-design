ddbefe39b5d951c363c17f55c3eea28a7bf9ea81
JaleRehimli0909 <rehimlijale0909@gmail.com>
2023-11-09 15:24:23 +0400

    About, Shop pages added

--- website1reactapp/src/pages/ShopApp/index.jsx
+++ website1reactapp/src/pages/ShopApp/index.jsx
@@ -1,10 +1,157 @@
-import React from 'react';
-import './style.css';
+import React from "react";
+import "./style.css";
+import { Button, Dropdown, Row, Col, Card } from "antd";
+
+// import RecentProducts from '../../components/RecentProducts';
+// import SaleProducts from '../../components/SaleProducts';
 
 const ShopApp = () => {
+  const products = [
+    {
+      key: "1",
+      image: require("../../assets/images/img1.jpg"),
+      title:
+        "Fairy Clean & Fresh Washing Up Liquid Pomegranate &amp; Honeysuckle 520 ml",
+      price: "£2.00",
+    },
+    {
+      key: "2",
+      image: require("../../assets/images/img2.jpg"),
+      title: "Colgate Triple Action Toothpaste 100ml",
+      price: "£1.50",
+    },
+    {
+      key: "3",
+      // image: require("../assets/images/img3.jpg"),
+      image: require("../../assets/images/img3.jpg"),
+      title: "Breeze Toilet Tissue Soft White 24 Roll",
+      price: "£5.70",
+    },
+    {
+      key: "4",
+      image: require("../../assets/images/img4.jpg"),
+      title:
+        "Comfort intense Fresh Sky Fabric Conditioner Liquid 60 Wash 900 ML",
+      price: "£4.50",
+    },
+    {
+      key: "5",
+      image: require("../../assets/images/img5.jpg"),
+      title: "Glens Vodka 70cl Extra smooth",
+      saleprice: "£20.00",
+      price: "£15.50",
+    },
+    {
+      key: "6",
+      image: require("../../assets/images/img6.jpg"),
+      title: "Pot Noodle Chicken & Mushroom Standard 90g",
+      saleprice: "£1.50",
+      price: "£0.90",
+    },
+    {
+      key: "7",
+      image: require("../../assets/images/img7.jpg"),
+      title: "Branston Baked Beans in a Rich and Tasty Tomato Sauce 4 x 410g",
+      saleprice: "£2.00",
+      price: "£1.50",
+    },
+    {
+      key: "8",
+      image: require("../../assets/images/img8.jpg"),
+      title: "Kelloggs Crunchy Nut Hazelnut & Chocolate Granola 380g",
+      saleprice: "£3.00",
+      price: "£2.50",
+    },
+  ];
+  const items = [
+    {
+      key: "1",
+      label: (
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.antgroup.com"
+        >
+          Featured
+        </a>
+      ),
+    },
+    {
+      key: "2",
+      label: (
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.aliyun.com"
+        >
+          Price: Low to High
+        </a>
+      ),
+    },
+    {
+      key: "3",
+      label: (
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.luohanacademy.com"
+        >
+          Price: High to Low
+        </a>
+      ),
+    },
+  ];
+
   return (
-    <div>ShopApp</div>
-  )
-}
+    <div className="ShopApp">
+      <div className="container">
+        <div className="ShopAppTitle-Holder">
+          <h2>SHOP</h2>
+          <Dropdown
+            menu={{
+              items,
+            }}
+            placement="bottom"
+            arrow
+          >
+            <Button>Sort by</Button>
+          </Dropdown>
+        </div>
+        {/* <RecentProducts/>
+        <SaleProducts/> */}
+
+        <Row gutter={[16, 16]}>
+          {products.map((product) => {
+            return (
+              <Col span={6} key={product.key}>
+                <Card
+                  hoverable
+                  style={{
+                    width: 275,
+                  }}
+                  cover={
+                    <img
+                      alt="example"
+                      className="productsImg"
+                      src={product.image}
+                    />
+                  }
+                >
+                  {/* <Meta title={product.title} /> */}
+
+                  <h3 className="productsTitle">{product.title}</h3>
+                  <p className="price">{product.price}</p>
+                  <Button className="cardButton" type="primary">
+                    Add to Basket
+                  </Button>
+                </Card>
+              </Col>
+            );
+          })}
+        </Row>
+      </div>
+    </div>
+  );
+};
 
 export default ShopApp;
