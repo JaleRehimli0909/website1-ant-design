e8b8202307a30153d8ab132e2bd33a10fa81503f
JaleRehimli0909 <rehimlijale0909@gmail.com>
2023-11-09 16:14:00 +0400

    Contact page added

--- website1reactapp/src/pages/ContactApp/index.jsx
+++ website1reactapp/src/pages/ContactApp/index.jsx
@@ -1,10 +1,114 @@
-import React from 'react';
-import './style.css';
+import React from "react";
+import "./style.css";
+import { Button, Form, Input, Select } from "antd";
+const { Option } = Select;
+const { TextArea } = Input;
 
 const ContactApp = () => {
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+  const prefixSelector = (
+    <Form.Item name="prefix" noStyle>
+      <Select
+        style={{
+          width: 70,
+        }}
+      >
+        <Option value="86">+86</Option>
+        <Option value="87">+87</Option>
+      </Select>
+    </Form.Item>
+  );
   return (
-    <div>ContactApp</div>
-  )
-}
+    <div className="ContactApp">
+      <div className="container">
+        <h2>Contact</h2>
+        <Form
+          name="basic"
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+          layout="vertical"
+          size="large"
+        >
+          <Form.Item
+            label="Fullname"
+            name="fullname"
+            rules={[
+              {
+                required: true,
+                message: "Please input your fullname!",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Email;"
+            name="email"
+            rules={[
+              {
+                required: true,
+                message: "Please input your email!",
+              },
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+          <Form.Item
+            name="phone"
+            label="Phone Number"
+            rules={[
+              {
+                required: true,
+                message: "Please input your phone number!",
+              },
+            ]}
+          >
+            <Input
+              addonBefore={prefixSelector}
+              style={{
+                width: "100%",
+              }}
+            />
+          </Form.Item>
+          <Form.Item
+            label="Message"
+            name="message"
+            rules={[
+              {
+                required: true,
+                message: "Please input your message!",
+              },
+            ]}
+          >
+            <TextArea rows={4} />
+          </Form.Item>
+
+          <Form.Item
+            wrapperCol={
+              {
+                // offset: 8,
+                // span: 16,
+              }
+            }
+          >
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
+  );
+};
 
 export default ContactApp;
